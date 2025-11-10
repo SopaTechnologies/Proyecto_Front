1051cfaaedee2625f1b523dd3ab337008a099ebb
Kendall <kcordero0511@gmail.com>
2025-11-10 16:17:34 -0600

    Cambios en el footer y sidebar, ademas de correcion de textos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -94,7 +94,7 @@ export const routes: Routes = [
         }
       },
        {
-  path: 'linetime',
+  path: 'Linea de tiempo',
   component: MensajesComponent,
   canActivate: [AuthGuard],
   data: {
