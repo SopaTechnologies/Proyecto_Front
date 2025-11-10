145b3fb2971147efcd4511a6b8c71a53abe707d6
miu <rvalverded@ucenfotec.ac.cr>
2025-11-10 16:57:19 -0600

    Ya funciona la edicion, pero no muesta los datos correctamente

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,34 +1,79 @@
-import { Component, inject, ViewChild} from '@angular/core';
-import { ProfileService } from '../../services/profile.service';
-import { CommonModule } from '@angular/common';
-import { FormsModule, NgModel} from '@angular/forms';
-import { IUser } from '../../interfaces';
-
+import { Component, inject, ViewChild } from "@angular/core";
+import { ProfileService } from "../../services/profile.service";
+import { CommonModule } from "@angular/common";
+import { FormsModule, NgModel } from "@angular/forms";
+import { IUser } from "../../interfaces";
+import Swal from "sweetalert2";
+import { UserService } from "../../services/user.service";
 @Component({
-  selector: 'app-profile',
+  selector: "app-profile",
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-  ],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  imports: [CommonModule, FormsModule],
+  templateUrl: "./profile.component.html",
+  styleUrl: "./profile.component.scss",
 })
 export class ProfileComponent {
   public profileService = inject(ProfileService);
 
-   @ViewChild("email") emailModel!: NgModel;
-    @ViewChild("password") passwordModel!: NgModel;
+  public userService = inject(UserService);
+
+  @ViewChild("email") emailModel!: NgModel;
+  @ViewChild("password") passwordModel!: NgModel;
 
   public userForm: Partial<IUser> = {
-    name: '',
-    lastname: '',
-    email: ''
+    name: "",
+    lastname: "",
+    email: "",
+    photo: "",
+    username: ""
   };
 
+  previewUrl: string | null = null;
+  selectedFile: File | null = null;
+  isUploadingPhoto: boolean = false;
+
+  onPhotoSelected(event: any) {
+    const file = event.target.files[0];
+    this.selectedFile = file;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.previewUrl = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+  confirmPhotoUpload() {
+    if (!this.selectedFile) return;
+
+    this.isUploadingPhoto = true;
+
+    const formData = new FormData();
+    formData.append("file", this.selectedFile);
+    formData.append("upload_preset", "user_photos_unsi");
+    formData.append("cloud_name", "dmbdlq4cx");
+
+    fetch("https://api.cloudinary.com/v1_1/dmbdlq4cx/image/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        this.userForm.photo = data.secure_url;
+        Swal.fire({
+          title: "Foto publicada!!",
+          text: "Foto subida correctamente!!",
+          icon: "success",
+        });
+      })
+      .catch((err) => console.error("Upload error:", err))
+      .finally(() => {
+        this.isUploadingPhoto = false;
+      });
+  }
+
   public passwordForm = {
-    email: '',
-    newPassword: ''
+    email: "",
+    newPassword: "",
   };
 
   constructor() {
@@ -39,19 +84,50 @@ export class ProfileComponent {
     setTimeout(() => {
       const user = this.profileService.user$();
       this.userForm = {
-        name: user.name || '',
-        lastname: user.lastname || '',
-        email: user.email || ''
+        name: user.name || "",
+        lastname: user.lastname || "",
+        email: user.email || "",
+        username: user.username || "",
       };
-      this.passwordForm.email = user.email || '';
+      this.passwordForm.email = user.email || "";
     }, 100);
   }
 
   updateUser() {
-    this.profileService.updateUser(this.userForm).subscribe();
+    this.userService.updateOrSave(this.userForm).subscribe({
+      next: (response: any) => {
+        Swal.fire({
+          title:
+            response.message || "Usuario guardado/actualizado correctamente!!",
+          text: "Operación completada exitosamente!!",
+          icon: "success",
+          confirmButtonText: "ok",
+        });
+      },
+      error: (err: any) => {
+        const rr = err.error?.description || err.message;
+        Swal.fire({
+          title: "Error",
+          text: rr,
+          icon: "error",
+        });
+      },
+    });
+    this.ngOnInit();
   }
 
   updatePassword() {
-    this.profileService.updatePassword(this.passwordForm.email, this.passwordForm.newPassword).subscribe();
+    const c = this.passwordForm.newPassword.length;
+    if (c < 8 || c > 16) {
+      Swal.fire({
+        title: "Error",
+        text: "La contraseña debe ser mayor o igual 8 caracteres y menor o igual a 16 !!",
+        icon: "warning",
+      });
+      return;
+    }
+    this.profileService
+      .updatePassword(this.passwordForm.email, this.passwordForm.newPassword)
+      .subscribe();
   }
 }
